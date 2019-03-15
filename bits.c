b054a54a0cc653082b1ff7534d71b00db017de71
Kepei Lei <klei@wpi.edu>
2019-03-15 16:42:03 -0400

    Question 11?

--- bits.c
+++ bits.c
@@ -374,7 +374,7 @@ int isAsciiDigit(int x) {
 int trueThreeFourths(int x) {
   int isNegMask = x >> 31;
   int roundOnes = isNegMask & 1;
-  int roundTows = isNegMask & 3;
+  int roundTows = isNegMask & 2;
   int divTwo = (x + roundOnes) >> 1;
   int divFour = (x + roundTows) >> 2;
   return divFour + divTwo;
